11175688fee82c5d7ea568e935b1a61768fbefc9
Clara Sanson <btsherwatching@gmail.com>
2024-06-20 16:15:12 -0300

    Desafio: hora da prática

---
+++ desafio.js
@@ -0,0 +1,24 @@
+//alert('Boas vindas ao nosso site!');
+
+//alert('Erro! Preencha todos os campos');
+
+//let nome = Lua;
+//let idade = 25;
+//let numeroDeVendas = 50;
+//let saldoDisponivel = 1000;
+//let mensagemDeErro = 'Erro! Preencha todos os campos!';
+
+//alert(mensagemDeErro);
+
+let nome = prompt ('Digite o seu nome');
+
+let idade = prompt ('Digite a sua idade');
+
+if(idade >= 18){
+    alert('Pode tirar a habilitação!');
+}else{
+    alert('Tem que estudar!');
+}
+
+
+
