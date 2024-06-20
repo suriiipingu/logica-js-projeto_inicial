caaf4c6fac747237f7eedc0985d5e87d56104492
Clara Sanson <btsherwatching@gmail.com>
2024-06-20 18:17:21 -0300

    Condicionais e concatenação

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ alert('Boas vindas ao jogo do número secreto!');
 // = é usado para atribuir; == é usado para igualar uma informação
 let numeroSecreto = 29;
 
+console.log(numeroSecreto); 
+//inclua comandos console.log em diferentes partes do código para verificar o fluxo do programa, os valores das variáveis e outras informações relevantes durante a fase de desenvolvimento.
+// mas retirar o console.log quando for compartilhar o programa, por conta de desempenho
+
 //caixa de texto para coletar a informação do usuário
 let chute = prompt('Escolha um número entre 1 e 30') ;
 
 //condição
 if(chute == numeroSecreto){
     //ambiente secreto para os devs :)
-    console.log('Isso ai! Você descobriu o número secreto!');
+    alert(`Isso ai! Você descobriu o número secreto: ${numeroSecreto}!`);
 }else{
-
+    alert('Você errou!');
 }
--- desafio.js
+++ desafio.js
@@ -10,15 +10,60 @@
 
 //alert(mensagemDeErro);
 
-let nome = prompt ('Digite o seu nome');
+//let nome = prompt ('Digite o seu nome');
 
-let idade = prompt ('Digite a sua idade');
+//let idade = prompt ('Digite a sua idade');
 
-if(idade >= 18){
-    alert('Pode tirar a habilitação!');
-}else{
-    alert('Tem que estudar!');
-}
+//if(idade >= 18){
+//    alert('Pode tirar a habilitação!');
+//}else{
+//    alert('Tem que estudar!');
+//}
 
 
 
+
+//Condicionais e concatenação
+
+//DESAFIO 1
+//let diaDaSemana = prompt('Qual é o dia da semana?');
+
+//if(diaDaSemana == 'Sábado' || diaDaSemana == 'sábado'|| diaDaSemana =='sabado'|| diaDaSemana =='Sabado' || diaDaSemana == 'domingo' || diaDaSemana == "Domingo"){
+//    alert('Bom fim de semana!');
+//}else{
+//    alert('Boa semana!');
+//}
+
+//DESAFIO 2 
+// let numeroDigitado = prompt('Digite um número negativo ou postivo');
+
+// if(numeroDigitado > 0){
+//     alert('Seu número é positivo');
+// }else if(numeroDigitado < 0){
+//     alert('Seu número é negativo');
+// }else{
+//     alert('Seu número é neutro ou é fração, raiz, entre outros OUUUU não é um número XD1/2');
+// }
+
+
+//DESAFIO 3 
+// while(true){ //Iniciar um loop
+//     let numero = prompt('Digite um número e veja sua sorte');
+
+// if(numero >=100){
+//     alert('Parabéns, você venceu!');
+//     break; //Sair do loop
+// }else{
+//     alert('Tente novamente para ganhar!');
+// }
+// }
+
+//DESAFIO 4
+// let valorSaldo = prompt('Qual o valor do saldo?');
+
+// alert(`O valor atual do saldo é: ${valorSaldo} reais`)
+
+//DESAFIO 5
+// let nomeUsuario = prompt('Digite seu nome');
+
+// alert(`Seja bem-vindo(a) ${nomeUsuario}!`);
